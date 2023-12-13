9c3a3030d6748d319432a6af9e836054cc6c0194
IShowLean <dobrikov.egor@mail.ru>
2023-12-14 02:34:12 +0300

    Movement and assets added

--- script.js
+++ script.js
@@ -1,49 +1,136 @@
-const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
-const DEFAULT_HEIGHT = 1080
-const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT
+const canvas = document.querySelector('canvas')
+const content = canvas.getContext('2d')
 
-const config = {
-    type: Phaser.AUTO,
-    physics: {
-        default: 'arcade',
-    },
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: DEFAULT_WIDTH,
-        height: DEFAULT_HEIGHT
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Station {
+    constructor() {
+        this.position = {
+            x: 1100,
+            y: 350
+        }
+
+        const image = new Image()
+        image.src = 'images/station.png'
+        image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+        }
     }
-};
 
-const game = new Phaser.Game(config);
+    draw() {
+        content.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
 
-
-function preload() {
-    this.load.image('station', 'station.png');
-    this.load.image('ship', 'ship.png');
-    this.load.image('space', 'space.jpg');
+    update() {
+        if (this.image) {
+            this.draw()
+        }
+    }
 }
 
-function create() {
-    this.add.image(0, 0, 'space').setOrigin(0, 0);
-    this.add.image(1300, 500, 'station').setScale(0.2);
-    this.add.image(400, 300, 'ship').setScale(0.5);
+class Player {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: canvas.height / 2
+        }
 
-    player = this.physics.add.sprite(100, 450, 'ship');
-    cursors = this.input.keyboard.createCursorKeys();
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
 
+        this.rotation = 0
+        this.backgroundImage = new Image();
+        this.backgroundImage.src = 'images/space.jpg';
+
+        const image = new Image()
+        image.src = 'images/ship.png'
+        image.onload = () => {
+            const scale = 0.35
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+        }
+    }
+
+    drawBackground() {
+        if (this.backgroundImage) {
+            content.drawImage(this.backgroundImage, 0, 0, canvas.width, canvas.height);
+        }
+    }
+
+    draw() {
+        content.save()
+        content.translate(player.position.x + player.width / 2, player.position.y + player.height / 2)
+        content.rotate(this.rotation)
+        content.translate(-player.position.x - player.width / 2, -player.position.y - player.height / 2)
+        content.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        content.restore()
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+        }
+    }
 }
 
-function update() {
+const player = new Player()
+const station = new Station()
 
+function animate() {
+    requestAnimationFrame(animate)
+    player.drawBackground();
+    station.update()
+    player.update()
+
+    if (player.position.x <= 0) {
+        player.velocity.x = 0
+        player.position.x = 0
+    }
+    else if (player.position.x + player.width >= canvas.width) {
+        player.velocity.x = 0
+        player.position.x = canvas.width - player.width
+    }
+
+    if (player.position.y <= 0) {
+        player.velocity.y = 0
+        player.position.y = 0
+    }
+    else if (player.position.y + player.height >= canvas.height) {
+        player.velocity.y = 0
+        player.position.y = canvas.height - player.height
+    }
 }
 
-function dockSpaceship() {
-}
+animate()
+
+addEventListener('keydown', ({ key }) => {
+    const velocityValue = 5
+    switch (key) {
+        case 'w':
+            player.velocity.y -= velocityValue
+            player.rotation = 0
+            player.rotation = 0
+            break;
+        case 'a':
+            player.velocity.x -= velocityValue
+            player.rotation = -1.6
+            break;
+        case 's':
+            player.velocity.y += velocityValue
+            player.rotation = 3.15
+            break;
+        case 'd':
+            player.velocity.x += velocityValue
+            player.rotation = 1.6
+            break;
+    }
+})
