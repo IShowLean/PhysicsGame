db73f5f97e82e67ea76ef68c2d12a9f82948fd5f
IShowLean <dobrikov.egor@mail.ru>
2023-12-14 19:35:46 +0300

    Decoration and improved movement added

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ const content = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+class Particle {
+    constructor({ position, velocity, radius, color, fades }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = radius
+        this.color = color
+        this.opacity = 1
+        this.fades = fades
+    }
+
+    draw() {
+        content.save()
+        content.globalAlpha = this.opacity
+        content.beginPath()
+        content.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        content.fillStyle = this.color
+        content.fill()
+        content.closePath()
+        content.restore()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.fades) this.opacity -= 0.01
+    }
+}
+
 class Station {
     constructor() {
         this.position = {
@@ -44,10 +74,6 @@ class Player {
             y: 0
         }
 
-        this.rotation = 0
-        this.backgroundImage = new Image();
-        this.backgroundImage.src = 'images/space.jpg';
-
         const image = new Image()
         image.src = 'images/ship.png'
         image.onload = () => {
@@ -55,12 +81,6 @@ class Player {
             this.image = image
             this.width = image.width * scale
             this.height = image.height * scale
-        }
-    }
-
-    drawBackground() {
-        if (this.backgroundImage) {
-            content.drawImage(this.backgroundImage, 0, 0, canvas.width, canvas.height);
         }
     }
 
@@ -82,12 +102,57 @@ class Player {
     }
 }
 
+class Background {
+    constructor(path) {
+        this.backgroundImage = new Image();
+        this.backgroundImage.src = path;
+    }
+
+    drawBackground() {
+        if (this.backgroundImage) {
+            content.drawImage(this.backgroundImage, 0, 0, canvas.width, canvas.height);
+        }
+    }
+}
+
 const player = new Player()
 const station = new Station()
+const lowerBackground = new Background('images/lower-background.png');
+const upperBackground = new Background('images/upper-background.png');
+const particles = []
+
+function createParticles() {
+    for (let i = 0; i < 200; i++) {
+        particles.push(new Particle({
+            position: {
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height
+            },
+            velocity: {
+                x: -0.5,
+                y: 0
+            },
+            radius: Math.random() * 3,
+            color: '#d3ffff'
+        }));
+    }
+}
+
+createParticles();
 
 function animate() {
     requestAnimationFrame(animate)
-    player.drawBackground();
+    content.fillStyle = 'black'
+    content.fillRect(0, 0, canvas.width, canvas.height)
+    upperBackground.drawBackground();
+    particles.forEach(particle => {
+        if (particle.position.x + particle.radius <= 0) {
+            particle.position.x = canvas.width + particle.radius;
+            particle.position.y = Math.random() * canvas.height;
+        }
+        particle.update()
+    })
+    lowerBackground.drawBackground();
     station.update()
     player.update()
 
@@ -112,25 +177,36 @@ function animate() {
 
 animate()
 
-addEventListener('keydown', ({ key }) => {
-    const velocityValue = 5
-    switch (key) {
-        case 'w':
-            player.velocity.y -= velocityValue
-            player.rotation = 0
-            player.rotation = 0
-            break;
-        case 'a':
-            player.velocity.x -= velocityValue
-            player.rotation = -1.6
-            break;
-        case 's':
-            player.velocity.y += velocityValue
-            player.rotation = 3.15
-            break;
-        case 'd':
-            player.velocity.x += velocityValue
-            player.rotation = 1.6
-            break;
+function handleDiagonalFlight(defaultValue) {
+    if (player.velocity.x < 0 && player.velocity.y < 0) {
+        player.rotation = -0.8
+    } else if (player.velocity.x > 0 && player.velocity.y < 0) {
+        player.rotation = 0.8
+    } else if (player.velocity.x < 0 && player.velocity.y > 0) {
+        player.rotation = -2.4
+    } else if (player.velocity.x > 0 && player.velocity.y > 0) {
+        player.rotation = 2.4
+    } else {
+        player.rotation = defaultValue;
     }
-})
+}
+
+addEventListener('keydown', ({ key }) => {
+    const velocityValue = 5;
+    if (['w', 'ц', 'ArrowUp'].includes(key)) {
+        player.velocity.y -= velocityValue;
+        handleDiagonalFlight(0)
+    }
+    else if (['a', 'ф', 'ArrowLeft'].includes(key)) {
+        player.velocity.x -= velocityValue;
+        handleDiagonalFlight(-1.57)
+    }
+    else if (['s', 'ы', 'ArrowDown'].includes(key)) {
+        player.velocity.y += velocityValue;
+        handleDiagonalFlight(3.15)
+    }
+    else if (['d', 'в', 'ArrowRight'].includes(key)) {
+        player.velocity.x += velocityValue;
+        handleDiagonalFlight(1.57)
+    }
+});
